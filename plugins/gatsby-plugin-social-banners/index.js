4d00d67a60111a994888270cf5dc2f45386e73c1
anuraghazra <hazru.anurag@gmail.com>
2019-11-06 19:14:18 +0530

    chore: cleaned up plugin, added npm preinstall script

--- plugins/gatsby-plugin-social-banners/index.js
+++ plugins/gatsby-plugin-social-banners/index.js
@@ -2,44 +2,29 @@ const path = require('path');
 const jimp = require('jimp');
 const dateformat = require('dateformat');
 
-module.exports = ({ markdownNode }) => {
+module.exports = async ({ markdownNode }) => {
   const { frontmatter, fields } = markdownNode;
   if (fields.posttype === 'case-studies') return;
 
   const output = path.join('./public', fields.slug, 'social-banner-img.jpg');
   // console.log(`markdownNode plugin:${JSON.stringify(markdownNode, null, 2)}`);
 
-  return Promise.all([
-    jimp.read(path.join(__dirname, 'social-banner-template.jpg')),
-    jimp.loadFont(path.join(__dirname, 'montserrat.fnt')),
-    jimp.loadFont(path.join(__dirname, 'karla-26-regular.fnt'))
-  ]).then(([image, montserrat, karla]) => {
-    image
-      .resize(1200, 630)
-      .print(
-        montserrat,
-        139,
-        231,
-        {
-          text: frontmatter.title,
-          alignmentX: jimp.HORIZONTAL_ALIGN_CENTER,
-          alignmentY: jimp.VERTICAL_ALIGN_MIDDLE
-        },
-        921,
-        179
-      )
-      .print(
-        karla,
-        0,
-        434,
-        {
-          text: `${dateformat(frontmatter.date, 'mmmm d, yyyy')}  |  Anurag Hazra`,
-          alignmentX: jimp.HORIZONTAL_ALIGN_CENTER,
-          alignmentY: jimp.VERTICAL_ALIGN_MIDDLE
-        },
-        1200,
-        44
-      )
-      .write(output);
-  });
+  const [image, montserrat, karla] = await Promise.all([
+    jimp.read(path.join(__dirname, './src/social-banner-template.jpg')),
+    jimp.loadFont(path.join(__dirname, './src/fonts/montserrat.fnt')),
+    jimp.loadFont(path.join(__dirname, './src/fonts/karla-26-regular.fnt'))
+  ]);
+  
+  image.resize(1200, 630)
+    .print(montserrat, 139, 231, {
+      text: frontmatter.title,
+      alignmentX: jimp.HORIZONTAL_ALIGN_CENTER,
+      alignmentY: jimp.VERTICAL_ALIGN_MIDDLE
+    }, 921, 179)
+    .print(karla, 0, 434, {
+      text: `${dateformat(frontmatter.date, 'mmmm d, yyyy')}  |  Anurag Hazra`,
+      alignmentX: jimp.HORIZONTAL_ALIGN_CENTER,
+      alignmentY: jimp.VERTICAL_ALIGN_MIDDLE
+    }, 1200, 44)
+    .write(output);
 };
