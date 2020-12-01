27e4f3bed4b687f05f09bab4cad9e9a12e3e34f6
Harsh Singh <harshhh.singh.personal@gmail.com>
2020-12-01 17:56:04 -0500

    Added no-select classNames.

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -46,15 +46,15 @@ const WordClouds = () => {
       aria-hidden="true"
       className="skills__word-clouds"
     >
-      <p style={{ top: 50, left: '100%', fontSize: 23 }}>HTML5</p>
-      <p style={{ top: 0, left: 0, fontSize: 25 }}>WebGL</p>
-      <p style={{ top: 200, left: -60, fontSize: 14 }}>CSS3</p>
-      <p style={{ top: '30%', left: '35%', fontSize: 18 }}>figma</p>
-      <p style={{ top: 40, left: '75%', fontSize: 12 }}>antd</p>
-      <p style={{ top: 380, left: '85%', fontSize: 18 }}>MongoDB</p>
-      <p style={{ top: 320, left: '65%', fontSize: 18 }}>Gatsby</p>
-      <p style={{ top: 350, left: 150, fontSize: 20 }}>ES6</p>
-      <p style={{ top: 120, left: '22%', fontSize: 12 }}>blender</p>
+      <p style={{ top: 50, left: '100%', fontSize: 23 }} className="no-select">HTML5</p>
+      <p style={{ top: 0, left: 0, fontSize: 25 }} className="no-select">WebGL</p>
+      <p style={{ top: 200, left: -60, fontSize: 14 }} className="no-select">CSS3</p>
+      <p style={{ top: '30%', left: '35%', fontSize: 18 }} className="no-select">figma</p>
+      <p style={{ top: 40, left: '75%', fontSize: 12 }} className="no-select">antd</p>
+      <p style={{ top: 380, left: '85%', fontSize: 18 }} className="no-select">MongoDB</p>
+      <p style={{ top: 320, left: '65%', fontSize: 18 }} className="no-select">Gatsby</p>
+      <p style={{ top: 350, left: 150, fontSize: 20 }} className="no-select">ES6</p>
+      <p style={{ top: 120, left: '22%', fontSize: 12 }} className="no-select">blender</p>
     </Parallax>
   );
 };
