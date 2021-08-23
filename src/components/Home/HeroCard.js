325a251683b1afc4eb9741d82c54e5657262a5ba
CronixZero <47929140+CronixZero@users.noreply.github.com>
2021-08-23 16:23:14 +0200

    Update HeroCard.js

--- src/components/Home/HeroCard.js
+++ src/components/Home/HeroCard.js
@@ -31,26 +31,22 @@ function copyToClipboard(str) {
 }
 
 export const CodeCard = () => {
-  const [text, setText] = useState(`new Date().getFullYear() - 2001;`);
-  const age = new Date().getFullYear() - 2001;
-  // trimed down polyfill of String.repeat
-  const changeText = () => {
-    let space = repeatString(' ', 54);
-    setText(age + ';' + space);
-  };
   return (
     <CodeCardWrapper>
       <pre>
-        1&nbsp;&nbsp;class <b>Person</b> {'{'}
+        1&nbsp;&nbsp;public class <b>CronixZero</b> {'{'}
       </pre>
-      <pre>2&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; constructor() {'{'}</pre>
+      <pre>&nbsp</pre>
+      <pre>private final String name;</pre>
+      <pre>private final String[] codingLanguages;</pre>
+      <pre>&nbsp</pre>
+      <pre>2&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; public CronixZero() {'{'}</pre>
       <pre>
-        3&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; this.name = "<b>Anurag Hazra</b>";
+        3&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; this.name = "<b>CronixZero</b>";
       </pre>
       <pre>
-        4&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; this.traits = ["<b>DESIGN</b>", "<b>DEV</b>"];
+        4&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; this.codingLanguages = ["<b>JAVA</b>", "<b>JAVASCRIPT</b>"];
       </pre>
-      <pre onClick={changeText}>5&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; this.age = {text}</pre>
       <pre>6&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {'}'}</pre>
       <pre>7&nbsp;&nbsp;{'}'}</pre>
     </CodeCardWrapper>
