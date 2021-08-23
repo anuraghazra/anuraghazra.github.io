b0552fc0f94ea6615a8f0806f3af48ee81f8669d
CronixZero <47929140+CronixZero@users.noreply.github.com>
2021-08-23 16:01:09 +0200

    Update index.js

--- plugins/gatsby-plugin-social-banners/index.js
+++ plugins/gatsby-plugin-social-banners/index.js
@@ -37,7 +37,7 @@ module.exports = async ({ markdownNode }) => {
         text: `${dateformat(
           frontmatter.date,
           'mmmm d, yyyy'
-        )}  |  Anurag Hazra`,
+        )}  |  CronixZero`,
         alignmentX: jimp.HORIZONTAL_ALIGN_CENTER,
         alignmentY: jimp.VERTICAL_ALIGN_MIDDLE,
       },
