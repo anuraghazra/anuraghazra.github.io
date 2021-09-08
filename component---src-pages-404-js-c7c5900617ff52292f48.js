e708bfcc8eafecac3d0e06579acc9195850b5353
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-09-08 18:25:36 +0000

    Deploy anuraghazra/anuraghazra.github.io to github.com/anuraghazra/anuraghazra.github.io.git:master

---
+++ component---src-pages-404-js-c7c5900617ff52292f48.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),o=t("yBb5"),r=t("vrFN"),c=t("+ZDr"),s=t.n(c);n.default=function(){return l.a.createElement(o.a,null,l.a.createElement(r.a,{title:"404: Not found"}),l.a.createElement("h1",null,"Opps, seems like you are lost!"),l.a.createElement("p",null,"Mind going back? or ",l.a.createElement(s.a,{to:"/blog"},"read some blogs")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-c7c5900617ff52292f48.js.map
